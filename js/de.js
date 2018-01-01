1421a6ff336be46ea0a1079a6f8ff389c8b87408
yuyanzhe <34998791+yuyanzhe@users.noreply.github.com>
2018-01-01 19:58:55 +0800

    Add files via upload

---
+++ js/de.js
@@ -0,0 +1,3 @@
+document.getElementById("de1").innerHTML='<a class="downloadBtn" href="https://pan.lanzou.com/i07927a" target="_blank">安装版下载</a>';
+document.getElementById("de2").innerHTML='<strong>下载软件前先关闭杀毒软件</strong>，安装后应用皮肤即可。';
+
